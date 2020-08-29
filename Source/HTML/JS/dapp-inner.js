342298a99247a0fc043dab40688ffc97eaad31fc
pev5691 <pev5691@yandex.ru>
2020-08-29 19:22:25 +0300

    Отредактировал TERA -> WBN в каталоге Source\HTML\JS

--- Source/HTML/JS/dapp-inner.js
+++ Source/HTML/JS/dapp-inner.js
@@ -1,12 +1,12 @@
 /*
- * @project: TERA
+ * @project: WellBeingNetwork
  * @version: Development (beta)
  * @license: MIT (not for evil)
  * @copyright: Yuriy Ivanov (Vtools) 2017-2020 [progr76@gmail.com]
- * Web: https://terafoundation.org
- * Twitter: https://twitter.com/terafoundation
- * Telegram:  https://t.me/terafoundation
-*/
+ * @copypaste: Evgeny Pustolenko (pev5691)  2019-2020 [pev5691@yandex.ru]
+ * Web: https://www.facebook.com/pev5691
+ * Telegram:  https://t.me/wellbeingnetwork
+ */
 
 var ALL_DATA_ONCE = 1;
 var SMART = {}, BASE_ACCOUNT = {}, INFO = {}, USER_ACCOUNT = [], USER_ACCOUNT_MAP = {}, OPEN_PATH = "", ACCOUNT_OPEN_NUM = 0;
--- Source/jinn/extlib/RBTree-test.js
+++ Source/jinn/extlib/RBTree-test.js
@@ -1,10 +1,12 @@
 /*
- * @project: JINN
- * @version: 1.0
+ * @project: WellBeingNetwork
+ * @version: Development (beta)
  * @license: MIT (not for evil)
- * @copyright: Yuriy Ivanov (Vtools) 2019-2020 [progr76@gmail.com]
- * Telegram:  https://t.me/progr76
-*/
+ * @copyright: Yuriy Ivanov (Vtools) 2017-2020 [progr76@gmail.com]
+ * @copypaste: Evgeny Pustolenko (pev5691)  2019-2020 [pev5691@yandex.ru]
+ * Web: https://www.facebook.com/pev5691
+ * Telegram:  https://t.me/wellbeingnetwork
+ */
 
 
 require("./RBTree");
