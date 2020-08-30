a32fd119f37acb1fa2cebcf427930125291d0be2
pev5691 <pev5691@yandex.ru>
2020-08-30 11:55:05 +0300

    Список для запуска кластера из нод

--- run-fork.js
+++ run-fork.js
@@ -14,8 +14,9 @@ global.START_NETWORK_DATE = 1598463131275; //formula of it value is (new Date(20
 global.CONSENSUS_PERIOD_TIME = 3000; //ms
 
 global.FORK_IP_LIST = [
-//    {"ip":"185.26.121.248","port":50005},   // hostland
+//    {"ip":"185.26.121.248","port":50005}, // hostland
     {"ip":"194.67.221.153","port":34734},   // ihor totoha
-//    {"ip":"188.227.85.27","port":50005},    // Server Space
-    {"ip":"194.147.78.188","port":34734}];  // Vdska 194.147.78.188
+//    {"ip":"188.227.85.27","port":50005},  // Server Space
+    {"ip":"194.147.78.188","port":34734}],  // Vdska 194.147.78.188
+    {"ip":"127.0.0.1","port":50006}],       // localhost
     {"ip":"194.147.78.188","port":50006}];  // Vdska 194.147.78.188 jinn
